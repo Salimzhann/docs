702f7fd011cee4730468ad0dd2c91dab5787ec9c
Salimzhann <manassalimzhan@icloud.com>
2023-07-27 13:29:08 +0600

    upload

---
+++ support.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("AIFit App  Support")
+st.write(
+    "Welcome to the Support page. Do you have any feature request, questions, or just want to chat? Reach us out at manassalimzhan04@gmail.com."
+)
